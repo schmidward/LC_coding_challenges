dcb6716780d0003ed0f4edba9b138fc5156ddbc8
schmidward <eric.d.schmid1@gmail.com>
2023-04-16 13:50:54 -0500

    Week 1 challenge completed

---
+++ Week1_Challenge.js
@@ -0,0 +1,6 @@
+// Area of a triangle challenge
+// Write a function that takes the base height of a triangle and return its area
+
+function triArea(base, height) {
+    return (base * height) * .5;
+}
